d3da2842898a98a658557c81e38b384bd608a5cd
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-04-15 06:55:58 +0200

    Update main.tf
    Personalised AWS Communication Server Configuration

--- Actions_workflow/main.tf
+++ Actions_workflow/main.tf
@@ -2,14 +2,14 @@
 
 
 data "aws_security_group" "existing" {
-  id = "sg-09be7f8b32ac66cc2" # Replace with the actual security group ID
+  id = "vpc-0a2850eb93b24e883" # Replace with the actual security group ID
 }
 
 
 resource "aws_instance" "communication_server" {
-  ami           = "ami-0715c1897453cabd1"  # Replace with your desired AMI ID
-  instance_type = "t2.small"
-  key_name      = "ChronisUva"
+  ami           = "ami-01dad638e8f31ab9a"  # Replace with your desired AMI ID
+  instance_type = "t3.micro"
+  key_name      = "server"
   
   tags = {
     "Name" = "Communication_server_terraform"
@@ -20,7 +20,7 @@ resource "aws_instance" "communication_server" {
   
 
   credit_specification {
-    cpu_credits = "unlimited"
+    cpu_credits = "standard"
   }
   
   user_data = <<-EOF
@@ -28,13 +28,13 @@ resource "aws_instance" "communication_server" {
       yum update -y
       yum install -y docker
       service docker start
-      docker pull chroniskaust/federation_server_endpoint_test:latest
-      sudo docker run -p 8088:8088 -d chroniskaust/federation_server_endpoint_test:latest 
+      docker pull anandanlk/communication_server:latest
+      sudo docker run -p 8088:8088 -d anandanlk/communication_server:latest 
   EOF
 
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 12  # Replace with your desired volume size in GB
+    volume_size           = 8  # Replace with your desired volume size in GB
     delete_on_termination = true
     iops                  = 3000  # Replace with your desired IOPS (optional)
   }
@@ -44,5 +44,3 @@ resource "aws_instance" "communication_server" {
   description = "CommunicationServer Public IP"
   value       = aws_instance.communication_server.public_ip
  }
-
-
