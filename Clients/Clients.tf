2d986ed1d3900960c3ad4ff7f4a2b5e7091df8e7
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-04-24 21:48:30 +0200

    Update Clients.tf

--- Clients/Clients.tf
+++ Clients/Clients.tf
@@ -36,8 +36,8 @@ resource "aws_instance" "client" {
     touch client_id${count.index + 1}.pt
     sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    sudo curl -L -O https://github.com/chronis98/Compose/raw/main/enumerated_tensors_5_version${count.index + 1}.pth
-    sudo curl -O https://raw.githubusercontent.com/chronis98/Compose/main/client_compose.yml
+    sudo curl -L -O https://github.com/anandanlk/Compose/raw/main/enumerated_tensors_5_version${count.index + 1}.pth
+    sudo curl -O https://raw.githubusercontent.com/anandanlk/Compose/main/client_compose.yml
     sudo docker-compose -f client_compose.yml up -d
   EOF
 
